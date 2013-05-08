9c01f73204b297e11a9f2fd7fe60290780f66d6f
Nikolay <oberon.engineer@gmail.com>
2013-05-08 13:32:02 +0700

    Implemented send packet

--- wakeproto.h
+++ wakeproto.h
@@ -1,9 +1,15 @@
 #include <QCoreApplication>
-
+#define FEND 0xC0
+#define FESC 0xDB
+#define TFEND 0xDC
+#define TFESC 0xDD
 class Wakeproto : public QObject
 {
 	Q_OBJECT
 	public:
 		Wakeproto();
 		void test();
+		QByteArray createpacket(unsigned char address, unsigned char cmd, QByteArray data);
+	private:
+		QByteArray stuffing(QByteArray packet);
 };
