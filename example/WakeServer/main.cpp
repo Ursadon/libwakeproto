d5cd64dd625520b7ad6b9ae9c16dca6a9ce67cd4
Ursadon <oberon.engineer@gmail.com>
2013-06-28 10:51:31 +0000

    NEW: WakeServer running in background

--- example/WakeServer/main.cpp
+++ example/WakeServer/main.cpp
@@ -1,11 +1,25 @@
 #include <QCoreApplication>
 #include <wakeproto.h>
 #include "wakeserver.h"
+#include <unistd.h>
+
+void daemonize()
+{
+    if(QCoreApplication::arguments().contains("--no-daemon"))
+        return;
+ 
+    switch (fork())
+    {
+        case 0:  break;
+        case -1: exit(-1);
+        default: exit(0);
+    }
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     WakeServer ws;
-
+    daemonize();
     return a.exec();
 }
